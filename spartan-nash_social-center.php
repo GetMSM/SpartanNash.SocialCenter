19774d4b8f15e6f060ab39cca996d779f72acd8f
IanDLacy <Ian@GetMSM.Com>
2019-06-28 14:19:32 -0400

    Fix For GitHub Updater ---------------------------------------------------------------- Branch: 	"Development" Changes: 	modified:   spartan-nash_social-center.php Notes: 	Fixed GitHub URL in plugin header to match renamed 	repository. 	The repository had to be renamed because GitHub Updater 	will not work with names containing "." characters.

--- spartan-nash_social-center.php
+++ spartan-nash_social-center.php
@@ -21,7 +21,7 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * 
- * GitHub Plugin URI: https://github.com/GetMSM/SpartanNash.SocialCenter
+ * GitHub Plugin URI: https://github.com/GetMSM/SpartanNash_SocialCenter
 */
 
 if ( ! defined( 'WPINC' ) )
